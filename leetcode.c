0d0928821c4a4e892dd7ce83fbc2163da1769362
Tsang <zwhex@live.com>
2020-06-26 09:19:00 +0800

    still tree order

--- leetcode.c
+++ leetcode.c
@@ -8,6 +8,23 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
+Tree* insertNode(Tree* t, int val)
+{
+    if(t==NULL){
+        t = (Tree*)malloc(sizeof(Tree));
+        t->val = val;
+        t->left = NULL;
+        t->right = NULL;
+    }
+    else if(val < t->val){
+        t-> left = insertNode(t->left, val);
+    }
+    else if(val > t->val){
+        t->right = insertNode(t->right, val);
+    }
+    return t;
+}
+
 void copyNode(Tree* t, Tree* nt)
 {
     if(t != NULL){
@@ -25,49 +42,51 @@ void copyNode(Tree* t, Tree* nt)
     }
 }
 
-Tree* copyTree(Tree* t)
+void copyTree(Tree** A, int* returnSize)
 {
-    Tree* nt = (Tree*)malloc(sizeof(Tree));
-    nt->left = NULL; nt->right = NULL;
-    copy(t, nt);
-    return nt;
+    // extend A size, and copy last tree in A
+    (*returnSize)++;
+    A = (Tree**)realloc(A, (*returnSize)*sizeof(Tree*));
+
+    A[*returnSize-1] = (Tree*)malloc(sizeof(Tree));
+    A[*returnSize-1]->left = A[*returnSize-1]->right = NULL;
+    copy(A[*returnSize-2], A[*returnSize-1]);
 }
 
-void subTrees(int *a, int i, int j, int *returnSize)
+void subTrees(Tree** A, int m, int n, int *returnSize)
 {
-    if(i == j)
-        *returnSize++;
-    else{
-
+    if(m == n){
+        A[*returnSize-1] = insertNode(A[*returnSize-1], m);
     }
+    else{
+        for(int i = m; i <= n; i++){
+
+            if(i-1 >= m)
+                subTrees(A, m, i-1, &returnSize);
+            if(i+1 <= n)
+                subTrees(A, i+1, n, &returnSize);
+        }
+    }
+    return T;
 }
 
 struct TreeNode** generateTrees(int n, int* returnSize)
 {
-    int* a = (int*)malloc(n*sizeof(int));
-    for(int i = 1; i <= n; i++)
-        *(a+i) = i;
-
-    int sum = 0; //create list to store trees
     Tree** A = NULL;
-
+ 
     for(int i = 1; i <= n; i++){
         // loop every i as the top of tree
-        Tree* t = (Tree*)malloc(sizeof(Tree));
-        t->val = i;
-        t->left = NULL;
-        t->right = NULL;
+        (*returnSize)++;
+        A = (Tree**)realloc(A, (*returnSize)*sizeof(Tree*));
+        A[*returnSize-1] = insertNode(A[*returnSize-1], i);
 
-        sum++;
-        A = (Tree**)realloc(A, sizeof(Tree*));
-
-
-        if(i-1>=0)
-            subTrees(a, 0, i-1, &returnSize);
-        if(i+1<=n-1)
-            subTrees(a, i+1, n-1, &returnSize);
+        if(i-1>=1)
+            subTrees(A, 1, i-1, &returnSize);
+        if(i+1<=n)
+            subTrees(A, i+1, n, &returnSize);
     }
-
+    
+    return A;
 }
 
 
