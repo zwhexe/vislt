0caea42f78355e64e25683108aa597051cd45402
Tsang <zwhex@live.com>
2020-08-04 21:56:11 +0800

    start coding with cpp

---
+++ leet.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        
+    }
+};
+
+int main()
+{
+    string s = "aab";
+    string p = "*.b";
+    Solution sol;
+    bool ans = sol.isMatch(s, p);
+    cout << ans << endl;
+
+    return 0;
+}
