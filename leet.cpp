4416ab6e133ac8ae724e7de67e9363fe0ffe5372
zwhex <zwhex@live.com>
2020-11-22 15:32:29 +0800

    dfs push and pop

--- leet.cpp
+++ leet.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <numeric>
 #include <string>
 #include <vector>
 #include <unordered_map>
@@ -8,26 +9,25 @@ using namespace std;
 
 class Solution {
 public:
-    int combination(int i) {
-        
+    void dfs(int cur, vector<int>& arr, vector<vector<int>>& res,
+            vector<int>& candidates, int target) {
+        if(target < 0) return; // sum is beyond target
+        if(target == 0) {   // sum of arr equal to target
+            res.push_back(arr);
+            return;
+        }
+        for(int i = cur; i < candidates.size(); i++) {
+            arr.push_back(candidates[i]);
+            dfs(i, arr, res, candidates, target - candidates[i]);
+            arr.pop_back();
+        }
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        if(candidates.size()<1 || candidates.size()>30 || target>500 || target<1)
-            return {};
-        sort(candidates.begin(), candidates.end());
-        if(*candidates.begin() < 1 || *(candidates.end()-1) > 200)
-            return {};
-        
         vector<vector<int>> res;
-        for(int i = 0; i < res.size(); i++){
-            if(res[i] > target)
-                break;
-            vector<int> arr;
-            int sum = 0;
-
-        }
-
+        vector<int> arr;
+        dfs(0, arr, res, candidates, target);
+        return res;
     }
 };
 
@@ -37,7 +37,11 @@ int main()
     int target = 7;
     Solution sol;
     vector<vector<int>> res = sol.combinationSum(candidates, target);
+    for(auto a: res){
+        for(auto b: a)
+            cout << b << " ";
+        cout <<endl;
+    }
 
-    
     return 0;
 }
