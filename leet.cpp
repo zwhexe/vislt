5fc75b698340b6a192683c2ba1cc986c731318d2
zwhex <zwhex@live.com>
2021-02-07 17:46:32 +0800

    unordered set

--- leet.cpp
+++ leet.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_set>
 #include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
-    void swap(int &a, int &b) {
-        int tmp = a;
-        a = b;
-        b = tmp;
-    }
+    vector<int> vec;
+    unordered_set<int> uset;
 
     void backtrack(vector<vector<int>> &res, vector<int> &arr, int pos, int len) {
         if(arr.size() == len) {
@@ -19,6 +17,10 @@ public:
             return;
         }
         for(int i = pos; i < len; i++) {
+            if(i!=pos && vec[i] == vec[pos] && uset.count(vec[i]))
+                continue;
+            else
+                uset.emplace(vec[i]);
             swap(vec[pos], vec[i]);
             arr.push_back(vec[pos]);
             backtrack(res, arr, pos+1, len);
@@ -28,24 +30,24 @@ public:
     }
 
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        // arrange nums from small to large
         sort(nums.begin(), nums.end());
+        // copy nums to vec as global variable
         for(auto a: nums)
             vec.emplace_back(a);
+        // prepare backtrack parameters
         int N = (int)nums.size();
         vector<vector<int>> res;
         vector<int> arr;
         backtrack(res, arr, 0, N);
         return res;
     }
-
-private:
-    vector<int> vec;
 };
 
 
 int main()
 {
-    vector<int> nums {1,1,2};
+    vector<int> nums {1,2,2,1};
     Solution sol;
     vector<vector<int>> res = sol.permuteUnique(nums);
     for(auto r: res){
@@ -53,6 +55,6 @@ int main()
             cout << a << " ";
         cout << endl;
     }
-    cout << "---Program Ending---" << endl;
+    cout << "-----Program Ending-----" << endl;
     return 0;
 }
